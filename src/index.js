310208875aa3531b740aa17213b311dd724a596b
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-02-13 18:07:16 +0200

    добавляет кнопку стоп и атрибут проверки запуска

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
 
 /*Напиши скрипт, который после нажатия кнопки Start, раз в секунду меняет цвет фона body на случайное значение из массива используя инлайн-стиль. При нажатии на кнопку Stop, изменение цвета фона должно останавливаться.*/
 /*⚠️ Учти, на кнопку Start можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение темы запушено, кнопка Start была не активна.*/
 /*Для генерации случайного числа (индекс элемента массива цветов), используй функцию randomIntegerFromInterval.*/
 /*const randomIntegerFromInterval = (min, max) => {
-   return Math.floor(Math.random() * (max - min + 1) + min);
- };
- */
-const bodyRef = document.querySelector("body");
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  */
+const bodyRef = document.querySelector('body');
 const startBynRef = document.querySelector("[data-action='start']");
 const stopBynRef = document.querySelector("[data-action='stop']");
 
@@ -22,14 +22,33 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-startBynRef.addEventListener("click", () => {
-  const timerId = setInterval(() => {
-    const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+const switcher = {
+  switcherID: null,
+  isActive: false,
+  startSwitchHendler() {
+    if (this.isActive) {
+      return;
+    }
+    this.switcherID = setInterval(() => {
+      const randomColor =
+        colors[randomIntegerFromInterval(0, colors.length - 1)];
 
-    bodyRef.setAttribute("style", `background-color:${randomColor}`);
-  }, 1000);
-});
+      bodyRef.setAttribute('style', `background-color:${randomColor}`);
+    }, 1000);
+    this.isActive = true;
+  },
+  stoptSwitchHendler() {
+    clearInterval(this.switcherID);
+    this.isActive = false;
+  },
+};
 
-stopBynRef.addEventListener("click", () => {
-  clearInterval(timerId);
-});
+startBynRef.addEventListener(
+  'click',
+  switcher.startSwitchHendler.bind(switcher),
+);
+
+stopBynRef.addEventListener(
+  'click',
+  switcher.stoptSwitchHendler.bind(switcher),
+);
